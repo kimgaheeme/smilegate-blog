7673124f34e3f7674b751321cf53887c9783581e
GaheeKim <aqswefr@naver.com>
2022-12-15 13:25:56 +0900

    Feat: add repository DI
    ### [Feat]: [add repository DI]
    
    
    # 1. repository 의 DI를 위해 Module추가

--- src/BlogApp/app/src/main/java/com/smilegateblog/smliegateblog/di/InterfaceModule.kt
+++ src/BlogApp/app/src/main/java/com/smilegateblog/smliegateblog/di/InterfaceModule.kt
@@ -1,8 +1,17 @@
 package com.smilegateblog.smliegateblog.di
 
+import com.smilegateblog.smliegateblog.data.api.CommentApi
 import com.smilegateblog.smliegateblog.data.api.LoginApi
+import com.smilegateblog.smliegateblog.data.api.PostApi
+import com.smilegateblog.smliegateblog.data.api.ScrapApi
+import com.smilegateblog.smliegateblog.data.repository.CommentRepositoryImpl
 import com.smilegateblog.smliegateblog.data.repository.LoginRepositoryImpl
+import com.smilegateblog.smliegateblog.data.repository.PostRepositoryImpl
+import com.smilegateblog.smliegateblog.data.repository.ScrapRepositoryImpl
+import com.smilegateblog.smliegateblog.domain.repository.CommentRepository
 import com.smilegateblog.smliegateblog.domain.repository.LoginRepository
+import com.smilegateblog.smliegateblog.domain.repository.PostRepository
+import com.smilegateblog.smliegateblog.domain.repository.ScrapRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,5 +26,23 @@ class InterfaceModule {
     @Provides
     fun provideLoginRepository(loginApi: LoginApi) : LoginRepository {
         return LoginRepositoryImpl(loginApi)
+    }
+
+    @Singleton
+    @Provides
+    fun providePostRepository(postApi: PostApi) : PostRepository {
+        return PostRepositoryImpl(postApi)
+    }
+
+    @Singleton
+    @Provides
+    fun provideScrapRepository(scrapApi: ScrapApi) : ScrapRepository {
+        return ScrapRepositoryImpl(scrapApi)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCommentRepository(commentApi: CommentApi) : CommentRepository {
+        return CommentRepositoryImpl(commentApi)
     }
 }
