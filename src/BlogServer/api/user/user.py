a152078f07880e1de453eba83cf6e81c8061e696
GaheeKim <aqswefr@naver.com>
2022-12-16 16:39:56 +0900

    Fix: modify user api
    ### [Fix]: [modify user api]
    # 1. create user api 의 filter문 오류가 있어 수정

--- src/BlogServer/api/user/user.py
+++ src/BlogServer/api/user/user.py
@@ -26,13 +26,13 @@ now = datetime.now()
     }
 )
 async def create_user(userRequest: CreateUserRequest):
-    if sessionmaker.query(User).filter(User.email == userRequest.email).count() < 1:
+    if sessionmaker.query(User).filter(User.email == userRequest.email).filter(User.nickname == userRequest.nickname).count() < 1:
         add_user = User(email=userRequest.email, nickname=userRequest.nickname, created_at=now.date())
         sessionmaker.add(add_user)
         sessionmaker.flush()
         sessionmaker.commit()
         return CreateUserResponse(userId=add_user.user_id, email=add_user.email, nickname=add_user.nickname)
-    else :
+    else:
         user_object = sessionmaker.query(User) \
             .filter(User.nickname == userRequest.nickname) \
             .filter(User.email == userRequest.email) \
