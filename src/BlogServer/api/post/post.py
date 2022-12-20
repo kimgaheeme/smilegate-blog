b9eff6ea8a0bc6e62959267cab2a9ce831fc962e
GaheeKim <aqswefr@naver.com>
2022-12-21 00:33:01 +0900

    Fix: add post image
    ### [커밋 타입]: [작업내용]
    
    ##### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    
    # 1. 무엇을 수정했는지
    # 2. 왜 수정했는지
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    -
    #   [커밋 타입]  리스트
    #   feat      : 기능 (새로운 기능)
    #   fix       : 버그 (버그 수정)
    #   refactor  : 리팩토링
    #   style     : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs      : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore     : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post     : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    # ------------------
    #   [체크리스트]
    #     제목 첫 글자는 대문자로 작성했나요?
    #     제목은 명령문으로 작성했나요?
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분했나요?
    # ------------------

--- src/BlogServer/api/post/post.py
+++ src/BlogServer/api/post/post.py
@@ -32,7 +32,7 @@ now = datetime.now()
     },
     tags="Post"
 )
-async def create_post(userRequest: CreatePostRequest, userId: int, image: Optional[UploadFile] = File(None)):
+async def create_post(userRequest: CreatePostRequest, userId: int, image: UploadFile = File(...)):
     add_post = Post(
         user_id=userId,
         title=userRequest.title,
