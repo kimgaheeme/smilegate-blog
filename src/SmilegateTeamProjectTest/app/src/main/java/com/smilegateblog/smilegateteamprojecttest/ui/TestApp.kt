82c8f57ed93f0e2fd24b0bb7b95fa2325c71a37a
GaheeKim <aqswefr@naver.com>
2023-01-16 22:52:56 +0900

    feat: add di
    ### [커밋 타입]: [작업내용]
    
    ##### 본문 - 한 줄에 최대 72 글자까지만 입력하기
    
    # 1. 무엇을 수정했는지
    # 2. 왜 수정했는지
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    -
    #   [커밋 타입]  리스트
    #   feat      : 기능 (새로운 기능)
    #   fix       : 버그 (버그 수정)
    #   refactor  : 리팩토링
    #   style     : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs      : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore     : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post     : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    # ------------------
    #   [체크리스트]
    #     제목 첫 글자는 대문자로 작성했나요?
    #     제목은 명령문으로 작성했나요?
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분했나요?
    # ------------------

--- src/SmilegateTeamProjectTest/app/src/main/java/com/smilegateblog/smilegateteamprojecttest/ui/TestApp.kt
+++ src/SmilegateTeamProjectTest/app/src/main/java/com/smilegateblog/smilegateteamprojecttest/ui/TestApp.kt
@@ -1,6 +1,13 @@
 package com.smilegateblog.smilegateteamprojecttest.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.smilegateblog.smilegateteamprojecttest.ui.theme.SmilegateTeamProjectTestTheme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Composable
