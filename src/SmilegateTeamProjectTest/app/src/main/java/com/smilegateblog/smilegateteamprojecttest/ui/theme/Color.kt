a89de148131d1b94b86eb58620a748fdca0f42f2
GaheeKim <aqswefr@naver.com>
2023-01-25 16:15:25 +0900

    feat: add TextField
    1. chat에 image, camera 연결 X
    
    2. focus되어있을때 아닐때 구분해서 > or 카메라 앨범 하기

--- src/SmilegateTeamProjectTest/app/src/main/java/com/smilegateblog/smilegateteamprojecttest/ui/theme/Color.kt
+++ src/SmilegateTeamProjectTest/app/src/main/java/com/smilegateblog/smilegateteamprojecttest/ui/theme/Color.kt
@@ -7,5 +7,6 @@ val Purple700 = Color(0xFF3700B3)
 val Gray800 = Color(0xFF000000)
 val Gray100 = Color(0xFFFFFFFF)
 val Gray200 = Color(0xFFF6F6F6)
+val Gray400 = Color(0xFFB7B7B7)
 
 val Green100 = Color(0xFF5AD439)
